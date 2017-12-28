1781bf87c567968b9d60dbb5bec2c7fca9076a7a
Rafael José <rjsilvestre@gmail.com>
2017-12-28 14:34:43 +0800

    Replaced progress bar with percentage. Code arrangement

--- baccarat-sim.py
+++ baccarat-sim.py
@@ -2,6 +2,7 @@ import datetime
 from rules import Game
 
 def hand_values(hand):
+    """Creates a list of strings with the values of a hand."""
     values = []
     for i in range(3):
         try:
@@ -11,7 +12,7 @@ def hand_values(hand):
     return values
 
 num_decks = 8
-num_shoes = 1000
+num_shoes = 10000
 shoe_count = 0
 game_count = 0
 total_wins = {'banco': 0, 'punto': 0, 'tie': 0}
@@ -22,42 +23,48 @@ sim.create_shoe(num_decks)
 now = datetime.datetime.now()
 file_name = f'{num_decks}_{num_shoes}_{now.strftime("%d%m%y%H%M%S")}.txt'
 
-prog_full = '**********'
-prog_empty = '----------'
-
+# Open file
 with open(file_name, 'w') as sim_file:
 
-    print(' Generating:')
+    # Run through num_shoes
     for i in range(num_shoes):
+        shoe_wins = {'banco': 0, 'punto': 0, 'tie': 0}
         shoe_count += 1
         sim_file.write(f'\nShoe number {i + 1}\n\n')
-        shoe_wins = {'banco': 0, 'punto': 0, 'tie': 0}
 
+        # While the shoe has more than 5 cards
         while sim.num_cards >= 6:
             result = []
             game_count += 1
+
+            # Baccarat game
             sim.deal_hands()
             if not sim.is_natural():
                 sim.draw_thirds()
             game_result = sim.game_result()
             shoe_wins[game_result] += 1
             total_wins[game_result] += 1
+
+            # Append to results list
             result.append(game_result.title()[0])
             result.append(str(sim.banco_value))
             result.append(str(sim.punto_value))
             result.extend(hand_values(sim.banco_values))
             result.extend(hand_values(sim.punto_values))
             sim_file.write(','.join(result) + '\n')
-            progress = int((shoe_count / num_shoes) * 10)
-            print(f' [{prog_full[:progress] + prog_empty[progress:]}]', end='\r')
 
+            # Progress
+            progress = round((shoe_count / num_shoes) * 100, 1) 
+            print(f'Progress: {progress}%', end='\r')
+
+        # Shoe results
         sim_file.write('\nShoe results:\n')
         for win in shoe_wins:
             sim_file.write(f'{win.title()}:\t{shoe_wins[win]}\n')
         sim.create_shoe(num_decks)
 
+    # Total results
     sim_file.write('\nTotal results:\n')
     for win in total_wins:
         sim_file.write(f'{win.title()}:\t{total_wins[win]}\t\
 ({round((total_wins[win]/game_count) * 100, 4)}%)\n')
-print(f' Complete. Written to {file_name}')
